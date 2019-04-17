03ffec5053facce0ebb11e10e60678f52303f409
lfw3050 <lfw3050@dzkf.com>
2019-04-17 11:08:17 +0800

    Initial commit

---
+++ src/main/java/com/example/hystrixdashboard/HystrixDashboardApplication.java
@@ -0,0 +1,21 @@
+package com.example.hystrixdashboard;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+@SpringBootApplication
+public class HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+
+
+}
